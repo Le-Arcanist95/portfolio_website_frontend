0b07a7d49ac2528af7541479e951ce17db84789d
Levi Arcane <zarcanist95@gmail.com>
2023-04-03 12:25:42 -0400

    Commented out more temporary changes

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,7 @@
 // CSS Styling - Split background and webpage into two layers. The background layer would be a static image, and the webpage layer would be the dynamic content. Use a repeating pattern for the background image. Use tailwind.config.js for preset colors. The pattern should be subtle, and not distracting. Use values from tailwind.config.js for theme options.
 import React, { useContext } from 'react';
 import ThemeContext from '../context/ThemeProvider';
-import DataContext from '../context/DataProvider';
+// import DataContext from '../context/DataProvider';
 
 // import LoadingSpinner from '../components/LoadingSpinner';
 import Header from '../components/Header';
@@ -19,8 +19,7 @@ library.add(fab, fas);
 const Homepage = () => {
     // Destructure theme from ThemeContext
     const { theme } = useContext(ThemeContext);
-    const data = useContext(DataContext);
-    if (data.isLoaded) console.log(data.experience);
+    // const data = useContext(DataContext);
 
     // Set darkMode to Boolean value
     const darkMode = theme === 'dark' ? true : false;
